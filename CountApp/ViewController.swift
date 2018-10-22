9f828ca0717d0d9d88be384915e71dac226f3385
katouyo <yothiac13@gmail.com>
2018-10-22 10:59:03 +0900

    Initial Commit

---
+++ CountApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountApp
+//
+//  Created by Yoshiaki Kato on 2018/10/22.
+//  Copyright © 2018 Yoshiaki Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
